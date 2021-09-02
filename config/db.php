53131fd3e02a35c231416b2dd65bc6b5b926fe8a
Tim Kelty <tim@pixelandtonic.com>
2021-09-02 01:24:50 -0400

    Fix driver, port

--- config/db.php
+++ config/db.php
@@ -6,15 +6,16 @@
  * list of the available settings in vendor/craftcms/cms/src/config/DbConfig.php.
  */
 
+use craft\db\Connection;
 use craft\helpers\App;
 
 return [
-    'driver' => App::env('DB_DRIVER'),
+    'driver' => Connection::DRIVER_PGSQL,
     'server' => App::env('DB_SERVER'),
     'user' => App::env('DB_USER'),
     'password' => App::env('DB_PASSWORD'),
     'database' => App::env('DB_DATABASE'),
     'schema' => App::env('DB_SCHEMA') ?: 'public',
     'tablePrefix' => App::env('DB_TABLE_PREFIX'),
-    'port' => App::env('DB_PORT')
+    'port' => App::env('DB_PORT') ?: 5432,
 ];
