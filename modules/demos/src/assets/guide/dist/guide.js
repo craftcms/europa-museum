e3e572d3130b75e9d1058b4dc4db2098ab84dd56
Matt Stein <matt@pixelandtonic.com>
2022-07-21 11:10:18 -0700

    Fix scroll offset + leave notes.

--- modules/demos/src/assets/guide/dist/guide.js
+++ modules/demos/src/assets/guide/dist/guide.js
@@ -3,9 +3,10 @@ const subnav = document.getElementById('guide-subnav');
 const contentAnchorNodes = content.querySelectorAll("h2");
 const activeNavItem = subnav.querySelector('.active');
 
-let titles = [];
+let headings = [];
 let subnavItems = [];
 
+// Highlight currently-active *child* nav item depending on what’s in view
 const updateSubnavHighlight = () => {
     contentAnchorNodes.forEach((v,i)=> {
         let rect = v.getBoundingClientRect().y
@@ -17,18 +18,20 @@ const updateSubnavHighlight = () => {
     })
 }
 
+// Create links for h2 headings for use in subnav
 contentAnchorNodes.forEach((node) => {
-    titles.push({
+    headings.push({
         text: node.textContent.replace(' #', ''),
         href: node.querySelector('a').getAttribute('href')
     });
 });
 
-if (titles.length > 0) {
+if (headings.length > 0) {
+    // Create the subnav menu for h2 anchors
     let ul = document.createElement('ul');
     ul.className = 'subnav';
 
-    titles.forEach((item) => {
+    headings.forEach((item) => {
         let li = document.createElement('li');
         let a = document.createElement('a');
         let text = document.createTextNode(item.text);
@@ -43,7 +46,10 @@ if (titles.length > 0) {
 
     activeNavItem.appendChild(ul);
 
+    // Listen on scroll and keep the right subnav item highlighted
     window.addEventListener('scroll', updateSubnavHighlight);
+
+    // Do a quick highlight now, before any scrolling
     updateSubnavHighlight();
 }
 
