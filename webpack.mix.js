455be5c64b232037d2fc332ab48246c89f6bddb6
Tim Kelty <tim@craftcms.com>
2024-09-10 11:36:07 -0400

    Cloud

--- webpack.mix.js
+++ webpack.mix.js
@@ -32,26 +32,6 @@ mix
         whitelist: ['first-load', 'loading', 'is-ie', 'is-device', 'is-phone', 'is-desktop', 'lazypicture', 'lazyloading', 'lazyloaded', 'lazyfade', 'lazyblur', 'animated-gif', 'has-scroll-smooth', 'has-scroll-init', 'has-scroll-scrolling', 'has-scroll-dragging', 'c-scrollbar', 'c-scrollbar_thumb', 'is-inview', 'flickity-button', 'flickity-button-icon', 'previous', 'next', 'arrow', 'flickity-page-dots', 'is-selected', 'scrolled', 'page-404', 'page-503', 'page-error', '[data-router-wrapper]', '[data-page="home"]', '[data-page="exhibitions"]', '[data-page="visit"]', '[data-page="news"]', '[data-page="about"]', '[data-page="contact"]', '[data-router-view="exhibitions"]', '[data-router-view="visit"]', '[data-router-view="news"]', '[data-router-view="newsArticle"]', '[data-router-view="about"]', '[data-router-view="contact"]', '[data-router-view="styleguide"]', 'active', 'word', 'whiteText', 'exhibit-odd', 'exhibit-even', 'exhibit-1', 'exhibit-2', 'exhibit-3', 'exhibit-4', 'dark-ui', 'has-icon', 'no-blocks', 'isFirst', 'is-image', 'is-entry', 'is-highlighted', 'topBorder', 'layout-imageLeft', 'layout-imageFullWidth', 'layout-imageRight', 'narrowWidth', 'no-hero-image', 'figure']
     })
 
-    .criticalCss({
-        enabled: mix.inProduction(),
-        paths: {
-            base: process.env.DEFAULT_SITE_URL,
-            templates: './templates/_/',
-            suffix: '-critical.min'
-        },
-        urls: [
-            {
-                url: '/',
-                template: 'home'
-            },
-        ],
-        options: {
-            minify: true,
-            width: 1440,
-            height: 1200,
-        },
-    })
-
     .babelConfig({
         plugins: [
             '@babel/plugin-proposal-class-properties',
