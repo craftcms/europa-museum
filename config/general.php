8cc688955a28205d7e2a8ab7e7c75d8a78e7ff8d
August Miller <hello@gusmiller.com>
2025-11-21 15:28:02 -0800

    Remove custom backup command

--- config/general.php
+++ config/general.php
@@ -9,24 +9,9 @@
 use craft\config\GeneralConfig;
 use craft\helpers\App;
 
-// Removed --column-inserts to reduce size and prevent OOM errors
-$backupCommand = 'PGPASSWORD="{password}" pg_dump' .
-' --dbname={database}' .
-' --host={server}' .
-' --port={port}' .
-' --username={user}' .
-' --if-exists' .
-' --clean' .
-' --no-owner' .
-' --no-privileges' .
-' --no-acl' .
-' --file="{file}"' .
-' --schema={schema}';
-
 return GeneralConfig::create()
     ->omitScriptNameInUrls(true)
     ->allowUpdates(false)
-    ->backupCommand($backupCommand)
     ->defaultSearchTermOptions([
         'subLeft' => true,
         'subRight' => true,
