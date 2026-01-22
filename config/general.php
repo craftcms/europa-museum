ed62992c0a2fed8e2b108ccda53f7909aaab5359
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2026-01-22 12:07:04 +0000

    Tidy config settings

--- config/general.php
+++ config/general.php
@@ -23,29 +23,22 @@ $backupCommand = 'PGPASSWORD="{password}" pg_dump' .
 ' --schema={schema}';
 
 return [
-    '*' => [
-        'allowAdminChanges' => true,
-        'allowUpdates' => false,
-        'backupOnUpdate' => false,
-        'backupCommand' => $backupCommand,
-        'defaultSearchTermOptions' => [
-            'subLeft' => true,
-            'subRight' => true,
-        ],
-        'disallowRobots' => true,
-        'enableCsrfProtection' => true,
-        'useEmailAsUsername' => true,
-        'omitScriptNameInUrls' => true,
-        'maxInvalidLogins' => 1000,
-        'maxUploadFileSize' => 20000000,
-        'resourceBasePath' => dirname(__DIR__) . '/web/cpresources',
-        'maxSlugIncrement' => 100,
-        'aliases' => [
-            '@web' => App::env('DEFAULT_SITE_URL'),
-            '@assetBaseUrl' => App::env('S3_BASE_URL') ?: App::env('DEFAULT_SITE_URL'),
-        ],
+    'allowAdminChanges' => true,
+    'allowUpdates' => false,
+    'backupOnUpdate' => false,
+    'backupCommand' => $backupCommand,
+    'defaultSearchTermOptions' => [
+        'subLeft' => true,
+        'subRight' => true,
     ],
-    'dev' => [
-        'devMode' => true,
+    'disallowRobots' => true,
+    'enableCsrfProtection' => true,
+    'useEmailAsUsername' => true,
+    'omitScriptNameInUrls' => true,
+    'maxInvalidLogins' => 1000,
+    'maxUploadFileSize' => 20000000,
+    'maxSlugIncrement' => 100,
+    'aliases' => [
+        '@assetBaseUrl' => App::env('S3_BASE_URL') ?: App::env('DEFAULT_SITE_URL'),
     ],
 ];
